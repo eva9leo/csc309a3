8e9817a112a5c091ae0b8c33a73cf636fe20d706
Tiansheng Zhou <tiansheng.zhou@mail.utoronto.ca>
2017-12-03 22:13:32 -0500

    functions restored

--- front/js/login.js
+++ front/js/login.js
@@ -143,3 +143,143 @@ function regisFun(){
         }
     }
 }
+
+function actualLogin (name) {
+	logged_in = true;
+    document.getElementById("navId").innerHTML += "<li id = 'usr'><button id='usrbtn' onclick='myFunction()'> </button></li>";
+    document.getElementById("usrbtn").innerHTML = name+"<div class='popup'><button class='popuptext' id='myPopup' onclick='logout()'>Logout</button></div>";
+    document.getElementById("myintake").innerHTML = "My Intake";
+    document.getElementById("thirdView").innerHTML = "<div id = 'post-login-content'></div>"
+	
+	getLocalDate()
+	console.log(currentDate.year + '/' + currentDate.month + '/' + currentDate.date);
+	
+	setUpRecordView();
+}
+
+function setUpRecordView() {
+			$('#post-login-content').append(
+				$('<div>', {
+				id: 'total-calories-display'
+			}));
+
+			$('#total-calories-display').append(
+				$('<h2>', {
+				text: 'Energy: 0 kCal, Protein: 0 g, Fat: 0 g, Carbohydrate: 0 g'
+			}));
+			
+			$('#post-login-content').append(
+				$('<div>', {
+				id: 'date-selection'
+			}));
+			
+			 setUpDateSelection();
+
+			$('#post-login-content').append(
+				$('<div>', {
+				class: 'view2_container',
+				id: 'record-container'
+			}));
+
+			$('#record-container').append(
+				$('<div>', {
+				class: 'itembox search_result',
+				id: 'record-list-div'
+			}));
+
+			$('#record-list-div').append(
+				$('<ul>', {
+				id: 'record-list'
+			}));
+
+			$('#record-container').append(
+				$('<div>', {
+				class: 'itembox food_info',
+				id: 'record-info'
+			}));
+
+			$('#record-info').append(
+				$('<h2>', {
+				class: 'food_info_title',
+				text: 'Nutritional value'
+			}));
+
+			$('#record-info').append(
+				$('<ul>', {
+				id: 'record-nutrition-ul'
+			}));
+}
+
+function setUpDateSelection() {
+	// Setup year selection
+	$('#date-selection').append('<select id="year-select" onchange="updateDate()">'  + '</select>');
+	
+	for (var year = (currentDate.year - 5); year <= (currentDate.year + 5); year++) {
+		
+		if (year == currentDate.year) {
+			$('#year-select').append(
+				'<option value=' + year + ' selected>' + year + '</option>'
+			);
+		} else {
+			$('#year-select').append(
+				'<option value=' + year + '>' + year + '</option>'
+			);
+		}
+		
+	}
+	
+	// Setup months selection
+	$('#date-selection').append('<select id="month-select" onchange="updateDate()">'  + '</select>');
+	$('#date-selection').append('<select id="date-select">'  + '</select>');
+	
+	for (var month = 1; month <= 12; month++) {
+		
+		if (month == currentDate.month) {
+			$('#month-select').append(
+				'<option value=' + month + ' selected>' + month + '</option>'
+			);
+		} else {
+			$('#month-select').append(
+				'<option value=' + month + ' >' + month + '</option>'
+			);
+		}
+		
+	}
+	
+	// Setup dates selection
+	updateDate();
+	
+	$('#date-selection').append('<button id="change-date">Change Date</button>')
+}
+
+function updateDate(){
+
+	var month = Number(document.getElementById('month-select').value);
+	var year = Number(document.getElementById('year-select').value);
+	
+	document.getElementById('date-select').options.length = 0;
+	
+	var longmonths = [1,3,5,7,8,10,12];
+	
+	var total_dates = 0;
+	if (longmonths.indexOf(month) != -1) {
+		total_dates = 31;
+	}else if (month == 2){
+        if ((year % 4) == 0) {
+			total_dates = 29;
+		} else {
+ 			total_dates = 28;
+		}
+    } else {
+		total_dates = 30;
+	}
+	
+	for (var date = 1; date <= total_dates; date++) {
+		
+		if (date == currentDate.date) {
+			$('#date-select').append('<option value=' + date + ' selected>' + date + '</option>');
+		}else {
+			$('#date-select').append('<option value=' + date + ' >' + date + '</option>');
+		}
+	}
+}
